c0eafb405c6a8333477d8c766785c49986cd0b98
carrasqu <carrasqu@Juans-MacBook-Pro.local>
2019-07-29 11:42:22 -0400

    remove matplotlib

--- transformer2.py
+++ transformer2.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 from POVM import POVM
 import itertools as it
 import slicetf
